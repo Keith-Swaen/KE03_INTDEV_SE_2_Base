9427afdb5970ce00c5b2f5bffabfdfa85ef4e853
LebVi <lebvince@hotmail.com>
2025-06-05 14:47:34 +0200

    Productmodel toegevoegd met validatie-attributen

---
+++ KE03_INTDEV_SE_2_Base/Models/Product.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KE03_INTDEV_SE_2_Base.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int StockQuantity { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+} 
