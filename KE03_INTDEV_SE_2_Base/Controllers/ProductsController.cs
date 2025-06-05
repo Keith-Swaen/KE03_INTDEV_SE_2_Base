d9a7a1bc6bc81eb78e451a86d7cb19b9539301f0
LebVi <lebvince@hotmail.com>
2025-06-05 15:10:32 +0200

    Verwijderen mogelijk gemaakt in de controller

--- KE03_INTDEV_SE_2_Base/Controllers/ProductsController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/ProductsController.cs
@@ -21,13 +21,13 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
             return View(products);
         }
 
-        // GET: Producten/creëren
+        // GET: Producten/Creëren
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Producten/creëren 
+        // POST: Producten/Creëren
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create([Bind("Name,Description,Price")] Product product)
@@ -38,6 +38,36 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(product);
+        }
+
+        // GET: Producten/Verwijderen/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _productRepository.GetProductById(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Producten/Verwijderen/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product != null)
+            {
+                _productRepository.DeleteProduct(product);
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 } 
