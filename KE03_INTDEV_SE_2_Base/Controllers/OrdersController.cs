3b6f75a6babbd36d38bd779f53ec09c5b748f634
Lars <kleijnen.lars@gmail.com>
2025-06-06 10:53:01 +0200

    page created

---
+++ KE03_INTDEV_SE_2_Base/Controllers/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult OrderOverview()
+        {
+            return View();
+        }
+    }
+}
