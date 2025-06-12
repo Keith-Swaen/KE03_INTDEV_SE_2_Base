ce8193e666098ebb4d870d6170388e82308bb5a7
Keith Swaen <keithrandyswaen29@gmail.com>
2025-06-13 00:10:13 +0200

    Migrations fix

--- DataAccessLayer/Migrations/20250612180757_AddStockQuantityToProduct.cs
+++ DataAccessLayer/Migrations/20250612180757_AddStockQuantityToProduct.cs
@@ -10,13 +10,20 @@ namespace DataAccessLayer.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Products");
         }
     }
 }
