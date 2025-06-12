c7d1ed9b2d4e2993b9829d283fd5cc603392f045
Keith Swaen <keithrandyswaen29@gmail.com>
2025-06-12 23:31:28 +0200

    Feature van customer inactief zetten toegevoegd

--- KE03_INTDEV_SE_2_Base/Controllers/CustomersController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/CustomersController.cs
@@ -23,10 +23,16 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
         }
 
         // GET: Klanten
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(bool showInactive = false)
         {
-            _logger.LogInformation("Alle klanten worden opgehaald");
-            return View(await _context.Customers.ToListAsync());
+            _logger.LogInformation("Klanten worden opgehaald, showInactive: {ShowInactive}", showInactive);
+            ViewBag.ShowInactive = showInactive;
+            
+            var customers = showInactive 
+                ? await _context.Customers.ToListAsync()
+                : await _context.Customers.Where(c => c.Active).ToListAsync();
+                
+            return View(customers);
         }
 
         // GET: Klanten/Details/5
@@ -64,6 +70,7 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
         {
             if (ModelState.IsValid)
             {
+                customer.Active = true; // Ensure new customers are active by default
                 _logger.LogInformation("Nieuwe klant wordt aangemaakt: {CustomerName}", customer.Name);
                 _context.Add(customer);
                 await _context.SaveChangesAsync();
@@ -162,9 +169,11 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
             var customer = await _context.Customers.FindAsync(id);
             if (customer != null)
             {
-                _context.Customers.Remove(customer);
+                // Soft delete by setting Active to false
+                customer.Active = false;
+                _context.Update(customer);
                 await _context.SaveChangesAsync();
-                _logger.LogInformation("Klant succesvol verwijderd met ID: {CustomerId}", id);
+                _logger.LogInformation("Klant succesvol gedeactiveerd met ID: {CustomerId}", id);
             }
             else
             {
