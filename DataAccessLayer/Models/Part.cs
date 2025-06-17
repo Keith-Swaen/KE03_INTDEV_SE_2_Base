2acff1a29cf8b9573c6c8feb338e77252b4f6ba5
LebVi <lebvince@hotmail.com>
2025-06-17 18:02:36 +0200

    Update data modellen en interfaces om product categorieen te supporten

--- DataAccessLayer/Models/Part.cs
+++ DataAccessLayer/Models/Part.cs
@@ -10,10 +10,10 @@ namespace DataAccessLayer.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
-        public ICollection<Product> Products { get; } = new List<Product>();
+        public virtual ICollection<Product> Products { get; } = new List<Product>();
     }
 }
