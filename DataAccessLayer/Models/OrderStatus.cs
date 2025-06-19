7783987057c4b292666815568ff4236d826403b8
Lars <kleijnen.lars@gmail.com>
2025-06-19 12:43:53 +0200

    Order Status toegevoegd

---
+++ DataAccessLayer/Models/OrderStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    
+    public enum OrderStatus
+    {
+        InWacht = 0,
+        InBehandeling = 1,
+        Verzonden = 2,
+        Afgeleverd = 3,
+        Geannuleerd = 4,
+        Retour = 5
+    }
+
+    
+}
